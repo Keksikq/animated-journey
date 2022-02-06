eea28ee8d9802ced88398c7d220f2edcc0371d40
Keksikq <derix5508@gmail.com>
2022-02-06 15:01:50 +0300

    add main menu , some changes in phisics

--- Assets/Scripts/CharachterController.cs
+++ Assets/Scripts/CharachterController.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharachterController : MonoBehaviour
-{
-    public float speed;
+{   
+    private Rigidbody rb;
+    [SerializeField] private float speed;
     public SpawnMenager spawnMenager;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hMovement = Input.GetAxis("Horizontal") * speed / 2;
-        float vMovement = Input.GetAxis("Vertical") * speed;
-        transform.Translate(new Vector3( hMovement , 0 , vMovement)  * Time.deltaTime);
+        CarMovement();
+    }
+    private void CarMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal / 2, 0.0f, moveVertical);
+
+        // что бы скорость была стабильной в любом случае
+        // и учитывая что мы вызываем из FixedUpdate мы умножаем на fixedDeltaTimе
+        transform.Translate(movement * speed * Time.fixedDeltaTime);
     }
     private void OnTriggerEnter(Collider other) {
         spawnMenager.SpawnTriggerEnter();
---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    public void LoadRoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
